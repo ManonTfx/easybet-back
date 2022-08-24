367fc33972b4e2263bff42b3dabccb14e06a72cc
ManonTfx <65050086+ManonTfx@users.noreply.github.com>
2022-08-24 12:14:49 +0200

    feat: add front-end url cors

--- src/server.ts
+++ src/server.ts
@@ -46,7 +46,7 @@ async function startServer() {
       cors: {
         credentials: true,
         origin: [
-          process.env.FRONTEND_URL || 'http://localhost:3000',
+          'https://easybet-client-mhunzxyly-manontfx.vercel.app/ ' || 'http://localhost:3000',
           // TODO: remove development endpoints once the app is ready for production
           'https://studio.apollographql.com',
         ],
